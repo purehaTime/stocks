0ca56199717dde320ccc5bd3aedb5c9edd4ca255
Stanislav A <akimov.cta@gmail.com>
2019-05-13 23:43:54 +0300

    Start work with api stocks

--- src/app/main/companies/companies.component.ts
+++ src/app/main/companies/companies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-companies',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./companies.component.css']
 })
 export class CompaniesComponent implements OnInit {
+  @Input() stocksList = [];
 
   constructor() { }
 
--- src/app/main/constants.class.ts
+++ src/app/main/constants.class.ts
@@ -2,4 +2,9 @@ export class Constants {
     public static defaultStockList = [
       'AAPL', 'AMZN', 'GOOG', 'FB', 'MSFT'
     ];
+
+    public static baseApiLink = 'https://cloud.iexapis.com/stable';
+
+    public static batchStoks = '/stock/market/batch?';
+
 }
